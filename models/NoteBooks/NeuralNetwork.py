2cd60d3e28334ddbdf41f3db828eb3f5ec667b9f
Harrison <hmbillings98@gmail.com>
2020-02-27 10:42:06 -0700

    created app

--- models/NoteBooks/NeuralNetwork.py
+++ models/NoteBooks/NeuralNetwork.py
@@ -1,7 +1,8 @@
 import random
 import math
+import joblib
 import numpy as np
-from sklearn.externals import joblib
+
 
 
 class NeuralNetwork:
@@ -12,7 +13,7 @@ class NeuralNetwork:
         self.biases = self.__create_biases(arch)
         self.error = None
         self.learning_rate = learning_rate
-        self.activations = None
+        self.activations = None  # All neuron outputs from the network
         self.output = None
         self.training_set = None
         self.testing_set = None
@@ -20,7 +21,7 @@ class NeuralNetwork:
 
     def __sigmoid(self, x):
         """This is the activation function for the network, it will
-            squishify the results of the network to be between 0 and 1.
+            squishify the results of the network to be between -1 and 1.
             It is non-linear  like all other activation functions"""
         return 1 / (1 + np.exp(-x))
 
@@ -105,6 +106,8 @@ class NeuralNetwork:
         # the last layer, we omit the first layer as that would calculate
         # the error for inputs which does not make sense
         for index, layer in enumerate(self.weights[:0:-1]):
+            # The error for a layer is the dot product of the transposed weight matrix
+            # and the error of the previous layer
             error.append(np.dot(layer.transpose(), error[index]))
         self.error = list(reversed(error))
 
@@ -161,8 +164,8 @@ class NeuralNetwork:
             to test accuracy. These lists are built from the list of all input pairs.
             This set of pairs will be constrained with a percentage
             of the total set. This percentage is represented as train_set_size. So for example
-            if this method is set to preform with a training set size of .7 then 
-            It will randomly select values from 70% of the list and reserve 30% to test on later. 
+            if this method is set to preform with a training set size of .7 then
+            It will randomly select values from 70% of the list and reserve 30% to test on later.
             """
         if train_set_size <= 0.0 or isinstance(train_set_size, int):
             raise ValueError('Training set size must be a positive floating point number')
@@ -192,7 +195,7 @@ class NeuralNetwork:
 
     @staticmethod
     def save_network(network, file_name):
-        joblib.dump(network, file_name)
+        joblib.dump(network, file_name, compress=True)
 
     @staticmethod
     def load_network(file_name):
