8478894d743c378ab5aff265337d648943793ac5
Harrison <hmbillings98@gmail.com>
2020-03-26 11:21:00 -0700

    Backend sends data in correct format

--- backend/server/iris_predictor/views.py
+++ backend/server/iris_predictor/views.py
@@ -3,17 +3,19 @@ from .apps import IrisPredictorConfig
 from django.http import JsonResponse
 from rest_framework.views import APIView
 import random
+import numpy as np
 
 
 # Create your views here.
 class CallModel(APIView):
     def get(self, req):
-        if req.method == "GET":
+        if req.method == 'GET':
             choice = random.choice(IrisPredictorConfig.network.input_pairs)
             guess = IrisPredictorConfig.network.guess(choice[0])
             guess = guess.tolist()
             actual = choice[1].tolist()
-            response = {"Guessed": guess,
-                        "Actual": actual,
-                        "Inputs": choice[0]}
+            response = {'guessed': guess,
+                        'actual': actual,
+                        'inputs': choice[0],
+                        'correct': np.array_equal(guess, actual)}
             return JsonResponse(response)
